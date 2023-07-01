ca0e27a8c5629e5ad2651577956082fa33b1e8b1
Matheus Ataide <matheus.pinheiro08@gmail.com>
2023-07-01 13:51:22 -0300

    Camada de xadrez e impressão da borda

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,39 @@
 package boardgame;
 
+import chess.ChessPiece;
+
 public class Board {
 
+	private int rows;
+	private int columns;
+	private Piece[][]pieces;
+	
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
+		
+		
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+	public ChessPiece piece(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
